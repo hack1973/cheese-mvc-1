f596c5f9702433319a40ce40043d62fbc1778844
Michael Hacker <hack1973@swbell.net>
2019-04-08 17:28:31 -0500

    user signup part 1 refactored

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,14 +1,15 @@
 package org.launchcode.controllers;
 
-import org.launchcode.models.Cheese;
-import org.launchcode.models.CheeseData;
 import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 /**
  * Created by Hackman
@@ -18,7 +19,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("user")
 public class UserController {
 
-    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    // Request path: /user
+    @RequestMapping(value = "")
+    public String index(Model model) {
+
+        model.addAttribute("title", "New User");
+
+        return "user/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model) {
         model.addAttribute("title", "Register");
         model.addAttribute(new User());
@@ -26,16 +36,23 @@ public class UserController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @ModelAttribute User user, @RequestParam String verifyPassword) {
+    public String add(@ModelAttribute @Valid User newUser, Errors errors, Model model, @RequestParam String verifyPassword) {
+
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Register");
+            return "user/add";
+        }
+
         // if (user.getVerifyPassword() == (user.getPassword())) {
-        if (user.getPassword().equals(verifyPassword)) {
+        if (newUser.getPassword().equals(verifyPassword)) {
             model.addAttribute("title", "New User");
-            model.addAttribute("user", user);
+            model.addAttribute("user", newUser);
             return "user/index";
         }
-        user.setPassword("");
+        newUser.setPassword("");
         model.addAttribute("title", "Register");
-        model.addAttribute("user", user);
+        model.addAttribute("user", newUser);
         model.addAttribute("error", "Passwords don't match!");
         return "user/add";
     }
